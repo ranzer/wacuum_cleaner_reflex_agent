3a9e267171dfe216d6a53ffcc3648139b6618d09
Zoran Radovanovic <zoranrdc@yahoo.com>
2019-09-15 12:55:37 -0700

    Added test for the getNewPos method that tests whether clonePos method is called or not.

--- test/actuator.spec.js
+++ test/actuator.spec.js
@@ -1,15 +1,14 @@
-import { spy } from "sinon";
+import { spy, stub } from "sinon";
 import { expect } from "chai";
 import { Actuator } from "../src/actuator.js";
 import { OrientationEnum, TurnEnum } from "../src/enums.js";
 
 describe("Actuator", () => {
   let actr;
-  beforeEach(() => {
-    console.log("beforeEach");
-    actr = new Actuator();
-  });
+
   describe("moveTo", () => {
+    beforeEach(() => { actr = new Actuator(); });
+
     it("should return value that is equal to the function's argument", () => {
       let pos = { col: 1, row: 2 };
       let ret = actr.moveTo(pos);
@@ -19,14 +18,32 @@ describe("Actuator", () => {
   });
   describe("getNewPos", () => {
     let pos = { col: 2, row: 2 };
+    beforeEach(() => { actr = new Actuator(); });
 
-    it("should return the current position argument when turn is not either left or right", () => {
-      let turn = 100;
-      let spyMethod = spy(actr.clonePos);
-      let ret = actr.getNewPos(pos, null, turn);
+    describe("when turn is not either left or right", () => {
+      it("should return the current position argumentt", () => {
+        let turn = 100;
+        let spyMethod = spy(actr.clonePos);
+        let ret = actr.getNewPos(pos, null, turn);
 
-      expect(ret).to.be.equal(pos);
-      expect(spyMethod.notCalled).to.be.true;
+        expect(ret).to.be.equal(pos);
+        expect(spyMethod.notCalled).to.be.true;
+      });
+    });
+    describe("when turn is either left or right", () => {
+      before(() => { actr = new Actuator(); });
+
+      it("should call the clonePos method", () => {
+        let turn = TurnEnum.LEFT;
+        let fn = spy();
+        let clonePosStub = stub(actr, "clonePos").callsFake(() => {
+          fn();
+          return pos;
+        });
+        let ret = actr.getNewPos(pos, OrientationEnum.LEFT, turn);
+
+        expect(fn.calledOnce).to.be.true;
+      });
     });
   });
 });
