8191691e83c024f24717d2dbc3427e338767b8ea
Zoran Radovanovic <zoranrdc@yahoo.com>
2019-09-22 10:29:49 -0700

    Refactored getNewPos method, instead of switch/case block the getChangePositionAction method returns correct action which changes the agent's position.

--- src/actuator.js
+++ src/actuator.js
@@ -36,37 +36,18 @@ class Actuator {
 	clonePos(pos) {
 		return { row: pos.row, col: pos.col };
 	}
+	getChangePositionAction(orientation) {
+		return this.changePositionActions[orientation];
+	}
 	getNewPos(currentPos, currentOrientation, turn) {
 		if (turn != TurnEnum.LEFT && turn != TurnEnum.RIGHT) {
 			return currentPos;
 		}
-		let newPos = this.clonePos();
-		switch (currentOrientation) {
-			case OrientationEnum.LEFT: {
-				if (turn == TurnEnum.LEFT) { newPos.row--; }
-				else { newPos.row++; }
-				break;
-			}
-			case OrientationEnum.RIGHT: {
-				if (turn == TurnEnum.LEFT) { newPos.row++; }
-				else { newPos.row--; }
-				break;
-			}
-			case OrientationEnum.UP: {
-				if (turn == TurnEnum.LEFT) {
-					newPos.col--;
-				}
-				else { newPos.col++; }
-				break;
-			}
-			case OrientationEnum.DOWN: {
-				if (turn == TurnEnum.LEFT) { newPos.col++; }
-				else { newPos.col--; }
-				break;
-			}
-		}
-		//console.log("getNewPos for turn " + turn + " and orientation " + currentOrientation);
-		//console.dir(newPos);
+		let newPos = this.clonePos(currentPos);
+		let action = this.getChangePositionAction(currentOrientation);
+
+		action(turn, newPos);
+
 		return newPos;
 	}
 	suck(cb) {
